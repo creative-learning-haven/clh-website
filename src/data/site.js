c944f913a67cb9f2f89e1ce17da6c29c088cb691
Samuel Lee <sam@samlee.dev>
2025-09-02 01:08:16 -0400

    update main page

--- src/data/site.js
+++ src/data/site.js
@@ -6,9 +6,10 @@
 
 // Import images for activities
 import groupStudyImg from '../assets/group_study.avif';
-import groupPhotoImg from '../assets/group_photo.avif';
-import womenGroupImg from '../assets/P1090543.jpg';
-import backgroundImg from '../assets/background.svg';
+import libraryImg from '../assets/library.avif';
+import paintingImg from '../assets/painting.avif';
+import womanKnittingImg from '../assets/woman_knitting.avif';
+import paintingOfWomanImg from '../assets/painting_of_woman.avif';
 
 export const siteData = {
   company: {
@@ -36,9 +37,9 @@ export const siteData = {
   
   activities: [
     {
-      image: backgroundImg,
+      image: libraryImg,
       heading: "Community Libraries",
-      text: "We establish and maintain libraries and learning spaces run by community members, providing access to educational resources."
+      text: "We establish and maintain libraries and learning spaces run by community members, providing access to educational resources. When you enter one of our libraries, our library team explains the book categorization system, recommends books, and introduces new activities."
     },
     {
       image: groupStudyImg,
@@ -46,16 +47,20 @@ export const siteData = {
       text: `<p>Schools in the settlements can have as many as 300 students per teacher. Many primary schoolers don't get the literacy education they need. To fill the gap, we offer literacy classes for students in primary 3 to 5. We cover the alphabet, basic phonics, and use the <a href="https://readinguniverse.org/article/explore-teaching-topics/features-of-structured-literacy-instruction/what-is-structured-literacy" target="_blank">Structured Literacy</a> approach. This way, students can benefit from the books in our libraries and beyond.</p>`
     },
     {
-      image: groupPhotoImg,
-      heading: "Creative Arts",
-      text: `Visitors to our libraries want to draw, take photos, write, and tell stories. However, these activities have been historically undervalued in the settlements. We create space for art and creativity not only for the mental health benefits they provide, but to preserve the voices and stories of Imvepi and Rhino for future generations.`
+      image: paintingImg,
+      heading: "Creative Activities",
+      text: `Visitors to our libraries want to draw, take photos, write, and tell stories. However, these activities have been historically undervalued in the settlements. Our activities include drawing and painting, creative writing, video workshops and a media team. We create space for art and creativity not only for the mental health benefits they provide, but to preserve the voices and stories of Imvepi and Rhino for future generations.`
     },
     {
-      image: womenGroupImg,
+      image: womanKnittingImg,
       heading: "Women's Groups",
       text: `82% of refugees in the Imvepi and Rhino settlements are women and children. To address the needs of the women and girls in these communities, we have creative arts and income-generating activities for women. We will soon begin offering literacy classes for women as well.`
     },
-
+    {
+      image: paintingOfWomanImg,
+      heading: "Art Pen Pals",
+      text: "Students in Winchester, MA are paired with students in Imvepi Refugee Camp to exchange pen pal emails and art they create in response to shared prompts."
+    }
   ],
 };
 
